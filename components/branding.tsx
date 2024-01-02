80d51e6e1954391b79c006631ea0805a6cebe38d
Emmanuel Bauer <bauerem@student.ethz.ch>
2024-01-02 16:02:25 +0100

    Changed from VPN to House-of-Charts

---
+++ components/branding.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from "./logo";
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+function Branding(props: any) {
+
+    const textColor = 'black';
+
+
+
+    return (
+        <div className={`flex title-font font-medium items-center text-${textColor} mb-4 md:mb-0`}>
+            <Logo />
+            <div className="ml-3 text-xl">House of Charts</div>
+        </div>
+    )
+
+    return (
+        <Link href={""} className={`flex title-font font-medium items-center text-${textColor} mb-4 md:mb-0`}>
+            <Image src="/ace-branding.png" alt="Branding Image" width={200} height={50} />
+        </Link>
+    )
+}
+
+export default Branding;
