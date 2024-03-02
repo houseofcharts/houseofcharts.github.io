002e5eb285e2de4f0e46bbbbcddf145f0cfb25bf
Emmanuel Bauer <bauerem@student.ethz.ch>
2024-03-02 20:41:10 +0100

    Another Git Action fix

--- next.config.js
+++ next.config.js
@@ -6,14 +6,15 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  //distDir: 'build',
+  //images: {
+  //  unoptimized: true,
+  //},
   //BACK_TO_FULLSTACK: remove prefix in production
   // Use the prefix and basePath only in production (when deploying to GitHub Pages)
   //assetPrefix: isProd ? '/houseofcharts.github.io/' : '',
-  //basePath: isProd ? '/houseofcharts.github.io' : '',
+  basePath: '/houseofcharts.github.io'//isProd ? '/houseofcharts.github.io' : '',
 };
 
 module.exports = nextConfig;
